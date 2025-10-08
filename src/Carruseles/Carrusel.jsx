d7c7c088a2ae5e65506b5240d237f99087d39d73
Miguel <miguel.pachonaar@cun.edu.co>
2025-10-07 19:36:19 -0500

    cambios 7/10

--- src/Carruseles/Carrusel.jsx
+++ src/Carruseles/Carrusel.jsx
@@ -3,9 +3,17 @@ import '../estilos/inicio.css';
 const Carrusel = ()=>{
     return <>
         <div class="Carrusel-caja">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/03/Lenovo_Global_Corporate_Logo.png" alt="imagen1"/>
-            <img src="https://www.stringnet.pe/blog/wp-content/uploads/2021/12/tipos-de-gafas-de-realidad-virtual-para-eventos-virtuales-1-1024x576.png" alt="imagen1"/>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/ASUS_Corporate_Logo.svg/1280px-ASUS_Corporate_Logo.svg.png" alt="imagen1"/>
+            <div class="Carrusel-caja1">
+                <div class="Carrusel-caja2">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/03/Lenovo_Global_Corporate_Logo.png" alt="imagen1"/>
+                </div>
+                <div class="Carrusel-caja2">
+                    <img src="https://www.stringnet.pe/blog/wp-content/uploads/2021/12/tipos-de-gafas-de-realidad-virtual-para-eventos-virtuales-1-1024x576.png" alt="imagen1"/>
+                </div>
+                <div class="Carrusel-caja2">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/ASUS_Corporate_Logo.svg/1280px-ASUS_Corporate_Logo.svg.png" alt="imagen1"/>
+                </div>
+            </div>
         </div>
     </>
 }
