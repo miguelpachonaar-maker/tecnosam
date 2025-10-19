bcc8d4716e226a9269140d89fb679a4d3bed7e76
Miguel <miguel.pachonaar@cun.edu.co>
2025-10-19 05:36:30 -0500

    cambios miguel

---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom'; // 👈 Necesario para inyectar páginas
+import MenuNav from '../MenuNav/MenuNav';
+import PiePagina from '../footer/footer.jsx';
+
+function RootLayout() {
+  return (
+    <>
+        <header className="inicio-header">
+            <h2>Tech-eCommerce</h2>
+        </header>
+        {/* 1. Elemento Fijo: La navegación se renderiza SIEMPRE */}
+        <MenuNav /> 
+
+        <main>
+        {/* 2. El Router Inyecta la página que corresponda (Home, About, etc.) */}
+            <Outlet /> 
+        </main>
+
+        {/* Puedes poner un Footer fijo aquí también */}
+        <PiePagina />
+    </>
+  );
+}
+export default RootLayout;
